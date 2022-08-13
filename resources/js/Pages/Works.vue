7a46d65cd8ef1c4618d11de15b5bf9bad887516d
Paulos Tadesse <paulosta952@gmail.com>
2022-08-13 13:38:22 +0300

    :rocket: works page

--- resources/js/Pages/Works.vue
+++ resources/js/Pages/Works.vue
@@ -17,22 +17,22 @@
 					<div class="xl:w-1/2 space-y-4 bg-white p-9 shadow-2xl">
 						<p class="uppercase text-2xl font-bold font-poppins text-black">al group</p>
 						<hr class="w-1/12 border-2 border-black">
-						<p class="max-w-2xl font-roboto text-gray-500 text-sm tracking-wider">Anchored in its unique and effective leadership development curriculum and a global network, the AL Group has been
+						<p class="max-w-2xl font-roboto text-gray-700 text-sm tracking-wider">Anchored in its unique and effective leadership development curriculum and a global network, the AL Group has been
 							successfully producing and placing globally competitive African talent at a speed, scale and quality never before seen.
 							Using an experiential, technology-enabled, peer-to-peer learning model, the AL Group has been equipping and harnessing the
 							potential of the young African workforce to meaningfully engage with — and contribute to — African economies as leaders and
 							innovators.</p>
 						<div class="grid grid-cols-2 gap-1 tracking-wide font-poppins">
-							<span class="text-xs p-2 border">Local Marketing Strategy</span>		
-							<span class="text-xs p-2 border">Copywriting</span>		
-							<span class="text-xs p-2 border">Graphic Design</span>		
-							<span class="text-xs p-2 border">Web Design</span>		
-							<span class="text-xs p-2 border">Social Media Marketing</span>		
-							<span class="text-xs p-2 border">Digital Advertising</span>		
-							<span class="text-xs p-2 border">Search Engine Optimization (SEO)</span>		
-							<span class="text-xs p-2 border">Google Display Networks (GDN)</span>		
-							<span class="text-xs p-2 border">Call Center Support</span>		
-							<span class="text-xs p-2 border">Research & BI</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Local Marketing Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Copywriting</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Graphic Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Web Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Social Media Marketing</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Digital Advertising</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Search Engine Optimization (SEO)</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Google Display Networks (GDN)</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Call Center Support</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Research & BI</span>		
 						</div>
 						<div class="float-right py-4">
 							<button class="border items-center flex space-x-2 text-xs py-2 px-4 border-white bg-black text-white hover:scale-90 transition ease-in delay-100">
@@ -61,15 +61,15 @@
 					<div class="xl:w-1/2 space-y-4 bg-white p-9 shadow-2xl">
 						<p class="uppercase text-2xl font-bold font-poppins text-black">kana tv</p>
 						<hr class="w-1/12 border-2 border-black">
-						<p class="max-w-2xl font-roboto text-gray-500 text-sm tracking-wider">With a vision to be the nation’s leading entertainment television channel, Kana TV founders needed an unrivalled market entry
+						<p class="max-w-2xl font-roboto text-gray-700 text-sm tracking-wider">With a vision to be the nation’s leading entertainment television channel, Kana TV founders needed an unrivalled market entry
 						strategy coupled with a unique brand identity to take Ethiopia by a storm. Tasked with this mission, STRATIX took a design thinking mindset to successfully launch Ethiopia’s most loved TV station. Six years later, Kana’s brand and campaigns remain	etched in the minds of viewers across the country.</p>
 						<div class="grid grid-cols-2 gap-1 tracking-wide font-poppins">
-							<span class="text-xs p-2 border">Media Landscape Research & BI</span>		
-							<span class="text-xs p-2 border">Business Strategy</span>		
-							<span class="text-xs p-2 border">Content Strategy</span>		
-							<span class="text-xs p-2 border">Brand Identity Design</span>		
-							<span class="text-xs p-2 border">Brand Launch & Activation</span>		
-							<span class="text-xs p-2 border">Digital Media Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Media Landscape Research & BI</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Business Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Content Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Identity Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Launch & Activation</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Digital Media Strategy</span>		
 						</div>
 						<div class="float-right py-4">
 							<button class="border items-center flex space-x-2 text-xs py-2 px-4 border-white bg-black text-white hover:scale-90 transition ease-in delay-100">
@@ -98,22 +98,22 @@
 					<div class="xl:w-1/2 space-y-4 bg-white p-9 shadow-2xl">
 						<p class="uppercase text-2xl font-bold font-poppins text-black">ETHIOPIAN TOURISM BOARD</p>
 						<hr class="w-1/12 border-2 border-black">
-						<p class="max-w-2xl font-roboto text-gray-500 text-sm tracking-wider">When one thinks of Ethiopian tourism campaigns, the words 13 MONTHS OF SUNSHINE echo a long held belief that Ethiopia
+						<p class="max-w-2xl font-roboto text-gray-700 text-sm tracking-wider">When one thinks of Ethiopian tourism campaigns, the words 13 MONTHS OF SUNSHINE echo a long held belief that Ethiopia
 						has 13 months of sunshine. The Ethiopian tourism board wanted Ethiopia to be known for much more than that. So we were
 						tasked to design a new tourism brand strategy to differentiate Ethiopia from competitors including a proposed visual brand
 						identity concept and message, a brand manual that includes the visual brand identity, communication messages and suggested
 						applications as recommended procedures for legal registration and copywriting of the brand.</p>
 						<div class="grid grid-cols-2 gap-1 tracking-wide font-poppins">
-							<span class="text-xs p-2 border">Creative Strategy</span>		
-							<span class="text-xs p-2 border">Brand Strategy</span>		
-							<span class="text-xs p-2 border">Brand Architecture</span>		
-							<span class="text-xs p-2 border">Brand Expression</span>		
-							<span class="text-xs p-2 border">Brand Identity</span>		
-							<span class="text-xs p-2 border">Brand Identity Design</span>		
-							<span class="text-xs p-2 border">Marketing Collateral Design</span>		
-							<span class="text-xs p-2 border">Website Design</span>		
-							<span class="text-xs p-2 border">Consumer Insights</span>		
-							<span class="text-xs p-2 border">Copywriting</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Creative Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Architecture</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Expression</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Identity</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Brand Identity Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Marketing Collateral Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Website Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Consumer Insights</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Copywriting</span>		
 						</div>
 						<div class="float-right py-4">
 							<button class="border items-center flex space-x-2 text-xs py-2 px-4 border-white bg-black text-white hover:scale-90 transition ease-in delay-100">
@@ -142,21 +142,21 @@
 					<div class="xl:w-1/2 space-y-4 bg-white p-9 shadow-2xl">
 						<p class="uppercase text-2xl font-bold font-poppins text-black">DIAGEO – MALTA GUINNESS</p>
 						<hr class="w-1/12 border-2 border-black">
-						<p class="max-w-2xl font-roboto text-gray-500 text-sm tracking-wider">After Diageo acquired Meta Abo Brewery in 		2010, it was eager to expand its portfolio and enter the soft drink category. With
+						<p class="max-w-2xl font-roboto text-gray-700 text-sm tracking-wider">After Diageo acquired Meta Abo Brewery in 		2010, it was eager to expand its portfolio and enter the soft drink category. With
 							that in mind, Malta Guinness: a non-alcoholic malt beverage was to be introduced for the first time to Ethiopia.
 						</p>
-						<p class="max-w-2xl font-roboto text-gray-500 text-sm tracking-wider">
+						<p class="max-w-2xl font-roboto text-gray-700 text-sm tracking-wider">
 							Diageo wanted an integrated ATL and BTL marketing campaign to increase brand awareness; covering various part of the city
 							where target consumers frequent. Radio ads along with vibrant wall brandings were requested to reinforce the BTL campaign.
 							For the BTL campaign, Diageo wanted to have sampling promotions to drive across the functional benefits of the product.
 						</p>
 						<div class="grid grid-cols-2 gap-1 tracking-wide font-poppins">
-							<span class="text-xs p-2 border">Creative Strategy</span>		
-							<span class="text-xs p-2 border">Campaign Identity</span>				
-							<span class="text-xs p-2 border">Marketing Collateral Design</span>		
-							<span class="text-xs p-2 border">Regional Activations</span>		
-							<span class="text-xs p-2 border">Consumer Insights</span>		
-							<span class="text-xs p-2 border">Copywriting</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Creative Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Campaign Identity</span>				
+							<span class="text-xs p-2 border border-black text-gray-500">Marketing Collateral Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Regional Activations</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Consumer Insights</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Copywriting</span>		
 						</div>
 						<div class="float-right py-4">
 							<button class="border items-center flex space-x-2 text-xs py-2 px-4 border-white bg-black text-white hover:scale-90 transition ease-in delay-100">
@@ -186,17 +186,17 @@
 					<div class="xl:w-1/2 space-y-4 bg-white p-9 shadow-2xl">
 						<p class="uppercase text-2xl font-bold font-poppins text-black">DIAGEO – SMIRNOFF</p>
 						<hr class="w-1/12 border-2 border-black">
-						<p class="max-w-2xl font-roboto text-gray-500 text-sm tracking-wider">With rival Vodka brands aggressively cutting into Smirnoff’s share of the market, Diageo was keen to regain the high ground and
+						<p class="max-w-2xl font-roboto text-gray-700 text-sm tracking-wider">With rival Vodka brands aggressively cutting into Smirnoff’s share of the market, Diageo was keen to regain the high ground and
 						position Smirnoff as the beverage of choice for young adults at bars and entertainment outlets. </p>
 						<div class="grid grid-cols-2 gap-1 tracking-wide font-poppins">
-							<span class="text-xs p-2 border">Creative Strategy</span>		
-							<span class="text-xs p-2 border">Campaign Identity</span>		
-							<span class="text-xs p-2 border">Marketing Collateral Design</span>		
-							<span class="text-xs p-2 border">City Activations</span>		
-							<span class="text-xs p-2 border">Consumer Insights</span>		
-							<span class="text-xs p-2 border">Bar Tender Training</span>		
-							<span class="text-xs p-2 border">Logistics Planning</span>		
-							<span class="text-xs p-2 border">Digital Engagment</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Creative Strategy</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Campaign Identity</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Marketing Collateral Design</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">City Activations</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Consumer Insights</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Bar Tender Training</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Logistics Planning</span>		
+							<span class="text-xs p-2 border border-black text-gray-500">Digital Engagment</span>		
 						</div>
 						<div class="float-right py-4">
 							<button class="border items-center flex space-x-2 text-xs py-2 px-4 border-white bg-black text-white hover:scale-90 transition ease-in delay-100">
