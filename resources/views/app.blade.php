6f0a9f5c1fada1b53a24efd9a9789639e0fad840
Paulos Tadesse <paulosta952@gmail.com>
2022-08-14 09:09:22 +0300

    :rocket: add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/STRATIX_LOGO_WHITE_ON_BLACK.svg')}}">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-
+    
     {{-- custom fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
