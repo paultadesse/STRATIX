0069aed93603e8aef6f48c8ed8f02977852943e1
Paulos Tadesse <paulosta952@gmail.com>
2022-08-15 18:54:14 +0300

    :fire: text justification

--- resources/js/Pages/Services.vue
+++ resources/js/Pages/Services.vue
@@ -20,25 +20,25 @@
 					
 
 					<div class="xl:col-span-3 space-y-4">
-						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 border-l-4 border-black p-4">Brand identity is the true reflection that an organization portrays to its customers which includes the promise of the brand from the company to the customers assuring them of true value delivery. It is essentially a bridge built on trust and credibility between the company and the consumer.</p>
+						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 border-l-4 border-black xl:text-justify p-4">Brand identity is the true reflection that an organization portrays to its customers which includes the promise of the brand from the company to the customers assuring them of true value delivery. It is essentially a bridge built on trust and credibility between the company and the consumer.</p>
 					</div>
 
 					<div class="xl:col-span-4 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">01</span> BRAND PURPOSE</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">The North Star for how, where and why the brand shows up To help define brand purpose, we start by interrogating the brand's past, understanding its present and anticipating its future. Once the brand purpose is defined, the team is able to make better decisions more quickly and with more confidence. A strong brand purpose is the first step toward building brand value over time.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">The North Star for how, where and why the brand shows up To help define brand purpose, we start by interrogating the brand's past, understanding its present and anticipating its future. Once the brand purpose is defined, the team is able to make better decisions more quickly and with more confidence. A strong brand purpose is the first step toward building brand value over time.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">02</span> DEFINITIVE DESIGN</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Harnessing the branding power of distinctive design assets In this accelerated world, people make decisions in the blink of an eye, through sensory means, without thinking about it logically. Brand design - sonic and visual - is not a "nice to have." It's essential to building modern, growth-driven brands.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Harnessing the branding power of distinctive design assets In this accelerated world, people make decisions in the blink of an eye, through sensory means, without thinking about it logically. Brand design - sonic and visual - is not a "nice to have." It's essential to building modern, growth-driven brands.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">03</span> PEOPLE & PATTERNS</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Humanize the data by We use data to map behaviors and recognize patterns emerging in the world, both within the category and those that are specific to the business and the brand. With that knowledge, we uncover the most significant opportunities to maximize how and when people engage.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Humanize the data by We use data to map behaviors and recognize patterns emerging in the world, both within the category and those that are specific to the business and the brand. With that knowledge, we uncover the most significant opportunities to maximize how and when people engage.</p>
 					</div>
 
 			</div>
@@ -59,19 +59,19 @@
 					
 
 					<div class="xl:col-span-2 flex row-span-2 space-y-4">
-						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 my-auto  border-l-4 border-black p-4">In a landscape that’s growing increasingly complex, we’re able to help brands cut through the noise and have their digital presence stand out in a super competitive ecosystem. From strategic positioning to superlative performance, the right digital strategy is your North Star when it comes to achieving your goals.</p>
+						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 my-auto  border-l-4 border-black xl:text-justify p-4">In a landscape that’s growing increasingly complex, we’re able to help brands cut through the noise and have their digital presence stand out in a super competitive ecosystem. From strategic positioning to superlative performance, the right digital strategy is your North Star when it comes to achieving your goals.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">01</span> YOUR ULTIMATE DIGITAL STRATEGY PARTNER</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Being a full partner in your digital march to success, we approach your unique marketing needs from a completely fresh perspective. We take an agnostic approach and perform deep dives into all facets of the category to determine the ideal strategic approach based on your budget and goals.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Being a full partner in your digital march to success, we approach your unique marketing needs from a completely fresh perspective. We take an agnostic approach and perform deep dives into all facets of the category to determine the ideal strategic approach based on your budget and goals.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">02</span> ANY PROJECT – NO MATTER HOW AMBITIOUS</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">No project is too big, in fact we encourage clients to dream big and then make sure they have the digital strategy in place to execute on this. Whether you’re a large multinational brand or a high-growth startup, we’ll craft your ultimate, achievable, digital strategy. From complete brand launches to smaller new features with repositioning, there’s nothing we can’t handle. If you can imagine it, we can create it for you.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">No project is too big, in fact we encourage clients to dream big and then make sure they have the digital strategy in place to execute on this. Whether you’re a large multinational brand or a high-growth startup, we’ll craft your ultimate, achievable, digital strategy. From complete brand launches to smaller new features with repositioning, there’s nothing we can’t handle. If you can imagine it, we can create it for you.</p>
 					</div>
 
 			</div>
@@ -92,7 +92,7 @@
 					
 
 					<div class="xl:col-span-3 space-y-4">
-						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 border-l-4 border-black p-4">We know the social media ecosystem incredibly well. You’ll benefit from this knowledge and experience as we help you come up with the perfect
+						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 border-l-4 border-black xl:text-justify p-4">We know the social media ecosystem incredibly well. You’ll benefit from this knowledge and experience as we help you come up with the perfect
 						social media strategy and achieve your specific goals. Whether organic, paid, or both, we know how to create content that engages your users
 						and inspires them to take action with your brand. Everything we do has quantifiable KPIs and is aimed to achieve one thing and one thing only:
 						to get results. We send regular progress reports, including highlighting impressive numbers, areas of growth, and new opportunities.</p>
@@ -101,19 +101,19 @@
 					<div class="xl:col-span-4 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">01</span> SOCIAL CONTENT THAT GETS USERS EXCITED</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Success on social media starts with having the right strategy in place. This strategy is driven by numerous factors, including your product, goals, budgets, trends, target audience, and more. Creating a winning social media content strategy gives you a clear and actionable path to over-achieve your growth goals.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Success on social media starts with having the right strategy in place. This strategy is driven by numerous factors, including your product, goals, budgets, trends, target audience, and more. Creating a winning social media content strategy gives you a clear and actionable path to over-achieve your growth goals.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">02</span> OUTSHINE COMPETITORS</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Working with the STRATIX team, you’ll get a deep and comprehensive view of what your competitors are doing in terms of both organic and paid social media activity, including what’s working for them and what isn’t – and then incorporating these learnings into your own social strategy.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Working with the STRATIX team, you’ll get a deep and comprehensive view of what your competitors are doing in terms of both organic and paid social media activity, including what’s working for them and what isn’t – and then incorporating these learnings into your own social strategy.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">03</span> ULTIMATE ORGANIC & PAID SOCIAL STRATEGY</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Humanize the data by We use data to map behaviors and recognize patterns emerging in the world, both within the category and those that are specific to the business and the brand. With that knowledge, we uncover the most significant opportunities to maximize how and when people engage.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Humanize the data by We use data to map behaviors and recognize patterns emerging in the world, both within the category and those that are specific to the business and the brand. With that knowledge, we uncover the most significant opportunities to maximize how and when people engage.</p>
 					</div>
 			</div>
 		</div>
@@ -132,11 +132,11 @@
 				</div>
 			</div>	
 
-			<div class="p-2 xl:p-12 xl:w-11/12 grid xl:grid-cols-4 space-y-4 py-12 xl:gap-4 ">
+			<div class="p-2 xl:py-12 xl:w-11/12 grid xl:grid-cols-4 space-y-4 py-12 xl:gap-4 ">
 					
 
 					<div class="xl:col-span-3 space-y-4">
-						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 border-l-4 border-black p-4">A marketing video has the power to boost conversion rates by 80%, and it’s no surprise that 92% of marketers say that video is a critical
+						<p class="font-roboto first-letter:text-2xl tracking-wide text-gray-500 border-l-4 border-black xl:text-justify p-4">A marketing video has the power to boost conversion rates by 80%, and it’s no surprise that 92% of marketers say that video is a critical
 						part of their overall marketing strategy. Today, people don’t want to read lengthy articles or search for explanations – they just want to
 						press play, and see everything they need in one, awesome place. At STRATIX, we offer a wide range of video production options for
 						marketing; all planned and produced in-house.</p>
@@ -145,20 +145,20 @@
 					<div class="xl:col-span-4 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">01</span> TV COMMERCIALS</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Craft the perfect cross-platform campaigns, that work together throughout a user’s journey; from TV to social media. These campaigns consist of a series of different kinds of videos in multiple sizes, with differentiated messaging to suit distinct needs.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Craft the perfect cross-platform campaigns, that work together throughout a user’s journey; from TV to social media. These campaigns consist of a series of different kinds of videos in multiple sizes, with differentiated messaging to suit distinct needs.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">02</span> AMAZING CORPORATE VIDEOS</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">Put your best foot forward with a corporate video that’s an inspiring portrait of who your company really is. An effective corporate video allows people to connect to your brand, vision, and mission; while
+						<p class="font-roboto tracking-wide font-light xl:text-justify">Put your best foot forward with a corporate video that’s an inspiring portrait of who your company really is. An effective corporate video allows people to connect to your brand, vision, and mission; while
 						giving a personal touch with a lasting impact.</p>
 					</div>
 
 					<div class="xl:col-span-2 space-y-4">
 						<p class="uppercase font-bold tracking-wider"><span class="text-4xl">03</span> POWERFUL PERSONAL BRANDING / LIVE ACTION</p>
 						<hr class="w-8 border-2 border-black">
-						<p class="font-roboto tracking-wide font-light">There are few more authoritative pieces of content than authentic video stories told by real people. Social proof is the greatest influencer, and personal branding videos empower you to show off the incredible person behind the camera.</p>
+						<p class="font-roboto tracking-wide font-light xl:text-justify">There are few more authoritative pieces of content than authentic video stories told by real people. Social proof is the greatest influencer, and personal branding videos empower you to show off the incredible person behind the camera.</p>
 					</div>
 			</div>
 		</div>
